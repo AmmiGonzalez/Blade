1ddc3ddaa00bf1c8ff603b14d6305153fa251c29
Ammi González <64441015+AmmiGonzalez@users.noreply.github.com>
2022-10-31 16:53:16 -0600

    Agregación de tablas

---
+++ store-sa/app/Http/Controllers/EncabezadoFacturaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EncabezadoFactura;
+use Illuminate\Http\Request;
+
+class EncabezadoFacturaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EncabezadoFactura  $encabezadoFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EncabezadoFactura $encabezadoFactura)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EncabezadoFactura  $encabezadoFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EncabezadoFactura $encabezadoFactura)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EncabezadoFactura  $encabezadoFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EncabezadoFactura $encabezadoFactura)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EncabezadoFactura  $encabezadoFactura
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EncabezadoFactura $encabezadoFactura)
+    {
+        //
+    }
+}
---
+++ store-sa/app/Http/Controllers/SucursalProductoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SucursalProducto;
+use Illuminate\Http\Request;
+
+class SucursalProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SucursalProducto  $sucursalProducto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SucursalProducto $sucursalProducto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SucursalProducto  $sucursalProducto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SucursalProducto $sucursalProducto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SucursalProducto  $sucursalProducto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SucursalProducto $sucursalProducto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SucursalProducto  $sucursalProducto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SucursalProducto $sucursalProducto)
+    {
+        //
+    }
+}
